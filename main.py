37db4de42b27d0f94ade1e3c7a401aed4c9186b8
GhostNikolay <snl-10@mail.ru>
2024-09-02 21:30:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+my_dict = {'Mike' : 1990}
+print(my_dict)
+print(my_dict['Mike'])
+my_dict ['Sam'] = 1995
+my_dict['Kira'] = 1997
+my_dict.pop ('Sam')
+print(my_dict)
+my_set = {1, 7, 4, 1, 2, 7, 'Urban', False, (2, 4, 6), 'Urban'}
+print(my_set)
+my_set.add(5)
+my_set.add(3)
+my_set.discard(1)
+print(my_set)
